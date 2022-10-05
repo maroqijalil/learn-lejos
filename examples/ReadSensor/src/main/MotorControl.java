5913c6fd3f29cfdefd778eaca066334070d9758a
maroqijalil <jalilpro31@gmail.com>
2022-10-06 02:22:04 +0700

    feat: add sensor with motor

---
+++ examples/ReadSensor/src/main/MotorControl.java
@@ -0,0 +1,62 @@
+package main;
+
+import lejos.hardware.Button;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
+public class MotorControl {
+	RegulatedMotor rightMotor;
+	RegulatedMotor leftMotor;
+	
+	public MotorControl(Port motorAPort, Port motorBPort) {
+		rightMotor = new EV3LargeRegulatedMotor(motorAPort);
+		leftMotor = new EV3LargeRegulatedMotor(motorBPort);
+	}
+	
+	public void stop() {
+		rightMotor.stop();
+		leftMotor.stop();
+	}
+	
+	public void moveForwardUntil(int delay) {
+		rightMotor.forward();
+		leftMotor.forward();
+		Delay.msDelay(delay);
+	}
+	
+	public void moveForward() {
+		rightMotor.forward();
+		leftMotor.forward();
+	}
+	
+	private void sync() {
+		RegulatedMotor motors[] = {leftMotor};
+		rightMotor.synchronizeWith(motors);
+		rightMotor.startSynchronization();
+	}
+	
+	private void stopSync() {
+		rightMotor.endSynchronization();
+	}
+	
+	public void rotateLeft(int angle) {
+		for (int i = 0; i < angle; i++) {
+			sync();
+			rightMotor.rotate(10);
+			leftMotor.rotate(-10);
+			stopSync();
+		}
+	}
+	
+	public void rotateRight(int angle) {
+		for (int i = 0; i < angle; i++) {
+			sync();
+			rightMotor.rotate(-10);
+			leftMotor.rotate(10);
+			stopSync();
+		}
+	}
+}
--- examples/ReadSensor/src/main/ReadSensor.java
+++ examples/ReadSensor/src/main/ReadSensor.java
@@ -3,6 +3,7 @@ package main;
 import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.SensorModes;
@@ -13,35 +14,47 @@ public class ReadSensor {
 	DistanceSensorFilter sensor;
 	
 	public ReadSensor(Port port) {
+		this.initSensor(port, 1);
+	}
+	
+	public ReadSensor(Port port, int average) {
+		this.initSensor(port, average);
+	}
+	
+	private void initSensor(Port port, int average) {
 		SensorModes modes = new EV3UltrasonicSensor(port);
 		SampleProvider provider = modes.getMode("Distance");
-		sensor = new DistanceSensorFilter(provider, 5);
+		sensor = new DistanceSensorFilter(provider, average);
 	}
 	
 	public void printDistance() {
-		LCD.drawInt((int)(sensor.distance() * 1000), 0, 4);
-		Delay.msDelay(800);
+		LCD.drawInt((int)(sensor.distance() * 100), 0, 4);
+		Delay.msDelay(8);
 		LCD.clear();
 	}
 	
+	public boolean anyCollision(float distance) {
+		return (sensor.distance() * 100) <= distance;
+	}
+	
 	public static void main(String[] args) {
-		Port port = LocalEV3.get().getPort("S1");
-		SensorModes modes = new EV3UltrasonicSensor(port);
-		SampleProvider provider = modes.getMode("Distance");
-		float sample[] = new float[provider.sampleSize()];
-
-		while (true) {
-			provider.fetchSample(sample, 0);
-			LCD.drawInt((int)(sample[0] * 1000), 0, 4);
-			Delay.msDelay(8);
-			LCD.clear();
-			
-			if (Button.ESCAPE.isDown()) {
-				break;
-			}
-		}
-
 //		Port port = LocalEV3.get().getPort("S1");
+//		SensorModes modes = new EV3UltrasonicSensor(port);
+//		SampleProvider provider = modes.getMode("Distance");
+//		float sample[] = new float[provider.sampleSize()];
+//
+//		while (true) {
+//			provider.fetchSample(sample, 0);
+//			LCD.drawInt((int)(sample[0] * 1000), 0, 4);
+//			Delay.msDelay(8);
+//			LCD.clear();
+//			
+//			if (Button.ESCAPE.isDown()) {
+//				break;
+//			}
+//		}
+
+//		Port port = LocalEV3.get().getPort("S4");
 //		ReadSensor rS = new ReadSensor(port);
 //		
 //		while (true) {
@@ -51,5 +64,24 @@ public class ReadSensor {
 //				break;
 //			}
 //		}
+
+		Port port = LocalEV3.get().getPort("S4");
+		ReadSensor rS = new ReadSensor(port);
+		MotorControl mC = new MotorControl(MotorPort.A, MotorPort.D);
+		
+		while (true) {
+			if (rS.anyCollision(30)) {
+//				mC.stop();
+				mC.rotateRight(90);
+			} else {				
+				mC.moveForward();
+			}
+			
+			if (Button.ESCAPE.isDown()) {
+				break;
+			}
+			
+			rS.printDistance();
+		}
 	}
 }
